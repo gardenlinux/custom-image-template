d611729bd2cab25ed6fbea60670379f025e48ba9
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-08-26 11:39:55 +0200

    Include Garden Linux as a submodule

--- README.md
+++ README.md
@@ -6,8 +6,7 @@ This template repo can be used to build custom images based on Garden Linux.
 
 - Create a new repository based on this template
 - Clone your repository
-- Run `bootstrap.sh`
 - Create your own feature in the `features` directory
+  - You may use `features/custom` as a starting point
   - Refer to the [docs](https://github.com/gardenlinux/builder/blob/main/docs/features.md)
-- Build your image using the `build` script
-
+- Build your image using the `build` script, for example using `./build kvm-custom`
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,15 @@
 set -euo pipefail
 
 git submodule add https://github.com/gardenlinux/gardenlinux
+pushd gardenlinux
+git fetch --all
+git switch rel-1592
+popd
 FILES=( bin build get_commit get_repo get_timestamp get_version keyring.gpg)
 for f in "${FILES[@]}"; do
     ln -s gardenlinux/"$f" "$f"
 done
-FEATURES=( _boot _ignite _selinux _slim base cloud firewall log sap server ssh sssd kvm _dev)
+FEATURES=( _iso _secureboot bluefield cisSshd firewall nodejs sssd _bfpxe _nopkg _selinux chost cisSysctl gardener openstack stig _boot _oci _slim cis clamav gcp openstackbaremetal stigDev _build _oras aide cisAudit cloud gdch python vhost _curl _persistence ali cisModprobe container khost sap vmware _debug _prod aws cisOS example kvm sapmachine _dev _pxe azure cisPackages fedramp log server _ignite _readonly base cisPartition firecracker metal ssh)
 mkdir features
 pushd features
 for f in "${FEATURES[@]}"; do
